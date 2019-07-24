7ea39e572184b3804ebfbdbf62f69ee7c827ef24
Aditi Ohri <didiohri@gmail.com>
2019-07-24 22:33:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(element) {
-  return kittens.push("element");
+function destructivelyAppendKitten(name) {
+  return kittens.push(name);
 }
 
 function destructivelyPrependKitten(element) {
